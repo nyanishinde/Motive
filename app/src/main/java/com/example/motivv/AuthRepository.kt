60092a8ff07a0d73539e6f88ad1a0561544251e2
nyanishinde <nyanishinde10@gmail.com>
2025-05-02 11:26:34 +0530

    Added AuthRepository.kt

---
+++ app/src/main/java/com/example/motivv/AuthRepository.kt
@@ -0,0 +1,17 @@
+package com.example.motivv
+
+import android.R
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthRepository {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun register(email: String,password: String,callback:(Boolean, String?)-> Unit){
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener { task->
+                if(task.isSuccessful) callback(true,null)
+                else callback(false,task.exception?.message)
+            }
+    }
+}
